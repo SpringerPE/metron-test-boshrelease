eb5f757223fcca8dd8221d90c2d5f24238b19f77
Claudio Benfatto <claudio.benfatto@gmail.com>
2018-05-03 12:05:02 +0100

    refactor dispatcher code

--- src/github.com/jriguera/metron-throughput/receiver/dispatcher.go
+++ src/github.com/jriguera/metron-throughput/receiver/dispatcher.go
@@ -2,58 +2,54 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"runtime"
 	"sync"
+	"time"
 	//"log"
 	//"code.cloudfoundry.org/loggregator/plumbing/conversion"
 )
 
-
 type Dispatcher struct {
 	// A pool of workers channels that are registered with the dispatcher
-	workerPool chan chan Job
-	maxWorkers int
-	aliveSeconds int
+	maxWorkers   int
+	aliveSeconds time.Duration
 	// A buffered channel that we can send work requests on.
-	jobQueue chan Job
-	filterOrigin string
+	jobQueue        chan Job
+	filterOrigin    string
 	workerInstances []*Worker
-	wg *sync.WaitGroup
-	running bool
+	wg              *sync.WaitGroup
+	running         bool
+	metronEvents    uint64
 }
-
 
 // NewDispatcher creates, and returns a new Dispatcher object.
 func NewDispatcher(jobQueue chan Job, maxWorkers int) *Dispatcher {
 	var wg sync.WaitGroup
-	workerPool := make(chan chan Job, maxWorkers)
 	d := Dispatcher{
-		jobQueue: jobQueue,
-		maxWorkers: maxWorkers,
-		workerPool: workerPool,
+		jobQueue:        jobQueue,
+		maxWorkers:      maxWorkers,
 		workerInstances: make([]*Worker, maxWorkers),
-		wg: &wg,
-		running: false,
+		wg:              &wg,
+		running:         false,
+		metronEvents:    0,
 	}
 	return &d
 }
 
-
-func (d *Dispatcher) Run(filterOrigin string, aliveSeconds int) bool {
+func (d *Dispatcher) Run(filterOrigin string, aliveSeconds time.Duration) {
 	if !d.running {
 		d.aliveSeconds = aliveSeconds
 		for i := 0; i < d.maxWorkers; i++ {
-			worker := NewWorker(d.aliveSeconds, i, d.workerPool, d.wg)
+			worker := NewWorker(d.aliveSeconds, i, d.jobQueue, d.wg)
 			d.workerInstances[i] = worker
 			worker.Start()
+			d.wg.Add(1)
 		}
 		d.running = true
-		go d.dispatch(filterOrigin)
 	}
-	return d.running
+	d.wg.Wait()
+	d.running = false
 }
-
 
 func (d *Dispatcher) Stop() bool {
 	if d.running {
@@ -67,69 +63,15 @@ func (d *Dispatcher) Stop() bool {
 	return !d.running
 }
 
-
-func (d *Dispatcher) dispatch(filterOrigin string) {
-	for {
-		select {
-		case job := <-d.jobQueue:
-			// a job request has been received
-			//go func() {
-				v := job.Version
-				switch v {
-						case 1:
-							//fmt.Printf("> %s\n", job.PayloadV1)
-							if (*job.PayloadV1.Origin == filterOrigin) {
-								// try to obtain a worker job channel that is available.
-								// this will block until a worker is idle
-								//fmt.Printf("> %s\n", job.Payload.GetLogMessage())
-								workerJobQueue := <-d.workerPool
-								// dispatch the job to the worker job channel
-								workerJobQueue <- job
-							}
-						case 2:
-							//fmt.Printf("> %s\n", job.PayloadV2)
-							if (job.PayloadV2.GetTags()["origin"] == filterOrigin) {
-								// try to obtain a worker job channel that is available.
-								// this will block until a worker is idle
-								//fmt.Printf("> %s\n", job.Payload.GetLogMessage())
-								workerJobQueue := <-d.workerPool
-								// dispatch the job to the worker job channel
-							workerJobQueue <- job
-						}
-					}
-			//}
-		}
-	}
-}
-
-
-// Keeps waiting for all workers until they decide to stop
-func (d *Dispatcher) WaitStop() {
-	var doneCounter int = 0
-	if d.running {
-		for {
-			for i := 0; i < d.maxWorkers; i++ {
-				worker := d.workerInstances[i]
-				if worker.IsDone() {
-					doneCounter++
-				}
-			}
-			if doneCounter >= d.maxWorkers {
-				break
-			}
-			time.Sleep(1 * time.Second)
-		}
-		d.running = false
-	}
-}
-
-
 // This does not use atomic operations
 // Run it after Stop()!!!!!!!
-func (d *Dispatcher) Print(diodesNumber int) bool {
+func (d *Dispatcher) Print(diodesNumber int, dopplerIngress uint64) bool {
 	var startTime, endTime *time.Time
 	var totalCounter, totalErrors uint64 = 0, 0
-	if (d.running) {
+
+	metricCounters := make(map[string]uint64)
+
+	if d.running {
 		return false
 	}
 	fmt.Println("*** INFO:")
@@ -146,21 +88,36 @@ func (d *Dispatcher) Print(diodesNumber int) bool {
 				startTime = &worker.StartT
 			}
 		}
+
+		for metric, value := range worker.MetricsCounters {
+
+			_, ok := metricCounters[metric]
+
+			if !ok {
+				metricCounters[metric] = value
+			} else if worker.MetricsCounters[metric] > value {
+				metricCounters[metric] = value
+			}
+		}
+
 		totalCounter = totalCounter + worker.Counter()
 		totalErrors = totalErrors + worker.Errors()
 		worker.Print()
 	}
+
 	fmt.Printf("* Totals:\n")
+	for metric, value := range metricCounters {
+		fmt.Printf("  %s = %d\n", metric, value)
+	}
 	fmt.Printf("  Logs received = %d\n", totalCounter)
 	fmt.Printf("  Errors = %d\n", totalErrors)
 	fmt.Printf("  Start time = %s\n", startTime.Format(time.RFC1123))
 	fmt.Printf("  End time = %s\n", endTime.Format(time.RFC1123))
 	elapsed := endTime.Sub(*startTime)
 	fmt.Printf("  Elapsed seconds = %f s\n", elapsed.Seconds())
-	rate := float64(totalCounter)/elapsed.Seconds()
+	rate := float64(totalCounter) / elapsed.Seconds()
 	fmt.Printf("  Rate = %f\n", rate)
-	fmt.Printf("* STATS Date Time NumCPUs diodes Workers LogsReceived Errors Duration Rate(logs/s)\n")
-	fmt.Printf("--STATS %s %s %d %d %d %d %d %f %f\n", startTime.Format("01-02-2006"), startTime.Format("15:04:05"), runtime.NumCPU(), diodesNumber, d.maxWorkers, totalCounter, totalErrors, elapsed.Seconds(), rate)
+	fmt.Printf("* STATS Date Time NumCPUs diodes Workers LogsReceived Errors Duration Rate(logs/s) logSenderTotalMessagesRead grpc.sendErrorCount dropsondeUnmarshaller.logMessageTotal dopplerIngress\n")
+	fmt.Printf("--STATS %s %s %d %d %d %d %d %f %f %d %d %d %d\n", startTime.Format("01-02-2006"), startTime.Format("15:04:05"), runtime.NumCPU(), diodesNumber, d.maxWorkers, totalCounter, totalErrors, elapsed.Seconds(), rate, metricCounters["logSenderTotalMessagesRead"], metricCounters["grpc.sendErrorCount"], metricCounters["dropsondeUnmarshaller.logMessageTotal"], dopplerIngress)
 	return true
 }
-
